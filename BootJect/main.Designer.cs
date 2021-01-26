6025430a32f391f43ddfc30e3f5a46a934d1642d
AcaiBerii <69873653+AcaiBerii@users.noreply.github.com>
2021-01-26 18:01:19 -0500

    updated
    but bugfix still doesnt work :flushed:

---
+++ BootJect/main.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace BootJect
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.div = new System.Windows.Forms.Label();
+            this.x = new System.Windows.Forms.Label();
+            this.Open = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(12, 9);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(139, 39);
+            this.title.TabIndex = 0;
+            this.title.Text = "bootject";
+            // 
+            // div
+            // 
+            this.div.AutoSize = true;
+            this.div.Location = new System.Drawing.Point(157, 21);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(61, 13);
+            this.div.TabIndex = 1;
+            this.div.Text = "_________";
+            // 
+            // x
+            // 
+            this.x.AutoSize = true;
+            this.x.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.x.Location = new System.Drawing.Point(224, 21);
+            this.x.Name = "x";
+            this.x.Size = new System.Drawing.Size(26, 25);
+            this.x.TabIndex = 2;
+            this.x.Text = "X";
+            this.x.Click += new System.EventHandler(this.Close);
+            // 
+            // Open
+            // 
+            this.Open.AutoSize = true;
+            this.Open.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Open.Location = new System.Drawing.Point(13, 48);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(80, 31);
+            this.Open.TabIndex = 5;
+            this.Open.Text = "Inject";
+            this.Open.Click += new System.EventHandler(this.In);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(261, 102);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.x);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.title);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BootJect";
+            this.Load += new System.EventHandler(this.main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label div;
+        private System.Windows.Forms.Label x;
+        private System.Windows.Forms.Label Open;
+    }
+}
+
---
+++ BootJect/main.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Reloaded.Injector;
+using System.IO;
+
+namespace BootJect
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+        private void Close(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void In(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.InitialDirectory = "c:\\";
+                open.Filter = "Dll Cheats (*.dll)|*.dll";
+                open.RestoreDirectory = true;
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    string fullPath = open.FileName;
+                    string fileName = open.SafeFileName;
+                    string path = fullPath.Replace(fileName, "");
+                    try
+                    {
+                        Process[] proclist = Process.GetProcesses();
+                        foreach (Process pr in proclist)
+                        {
+                            if (pr.ProcessName.StartsWith("Minecraft"))
+                            {
+                                Injector inj = new Injector(pr);
+                                inj.Inject(fullPath);
+                                inj.Dispose();
+                                MessageBox.Show("DLL has been injected into Minecraft (using AutoConfig)", "BootJect", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        MessageBox.Show("An unexpected error occurred while injecting your dll, " + open.FileName + ". Try opening an issue on the BootJect GitHub page.", "Woag x1!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("You didn't select an item from the dialog. Select a valid .dll file and try again!", "Woag x0!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("An unexpected, critical error occured. Please open an error on our GitHub page IMMEDIATELY.", "Woag x2!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
